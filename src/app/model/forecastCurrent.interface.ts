fbdd1631c956016de0d711056149682690f63908
liudmyla.husar <liudmyla.husar@tigoenergy.com>
2018-01-10 22:31:36 +0200

    first commit

---
+++ src/app/model/forecastCurrent.interface.ts
@@ -0,0 +1,46 @@
+/**
+ * Created by lyudmila on 08.01.18.
+ */
+export interface Weather {
+  icon: string;
+  code: string;
+  description: string;
+}
+
+export interface WeatherData {
+  rh: number;
+  pod: string;
+  pres: number;
+  timezone: string;
+  weather: Weather;
+  country_code: string;
+  clouds: number;
+  vis: number;
+  wind_spd: number;
+  wind_cdir_full: string;
+  app_temp: number;
+  lon: number;
+  state_code: string;
+  ts: number;
+  elev_angle: number;
+  h_angle: number;
+  dewpt: number;
+  ob_time: string;
+  uv: number;
+  sunset: string;
+  sunrise: string;
+  city_name: string;
+  precip: number | string | null;
+  station: string;
+  lat: number;
+  dhi: number;
+  datetime: string;
+  temp: number;
+  wind_dir: number;
+  slp: number;
+  wind_cdir: string;
+}
+
+export interface ForecastCurrent {
+  data: WeatherData[];
+}
---
+++ src/app/services/config-app.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by lyudmila on 08.01.18.
+ */
+
+import { IAppConfig } from '../model/app-config.interface';
+
+export const AppConfig: IAppConfig = {
+  weatherbitAPIKey: '6334e6b1872047df91fa98196f2bf055'
+};
